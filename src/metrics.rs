b4bf5b0df4a07f0a2fad0b806fb43313fedfd20c
Alexandre Negrel <negrel.dev@protonmail.com>
2023-08-25 10:05:07 +0200

    move metrics to /admin

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,8 +1,10 @@
+use actix_web::http::StatusCode;
 use actix_web_prom::{PrometheusMetrics, PrometheusMetricsBuilder};
 
 pub fn setup_metrics() -> PrometheusMetrics {
     PrometheusMetricsBuilder::new(&env!("CARGO_PKG_NAME").replace('-', "_"))
-        .endpoint("/metrics")
+        .endpoint("/admin/metrics")
+        .exclude_status(StatusCode::NOT_FOUND)
         .build()
         .expect("Failed to build prometheus metrics")
 }
