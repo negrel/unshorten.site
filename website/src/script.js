49d48174210e5ed24eb5381f5c49a1d935e056f0
Alexandre Negrel <negrel.dev@protonmail.com>
2023-08-28 21:00:50 +0200

    add meta tags for SEO

--- website/src/script.js
+++ website/src/script.js
@@ -1,5 +1,5 @@
-const isLocalServer = false && window.location.hostname === 'localhost'
-const apiEndpoint = isLocalServer ? "http://localhost/api/v1" : "/api/v1"
+const isLocalServer = window.location.hostname === 'localhost'
+const apiEndpoint = "/api/v1"
 
 const html = {
   sizeIndicator: `<div class="flex items-center justify-center fixed top-0 right-0 mt-12 mr-8 z-50 w-8 h-8 rounded-full text-gray-700 text-sm uppercase bg-gray-200 sm:bg-orange-200 md:bg-green-200 lg:bg-purple-200 xl:bg-red-200">
